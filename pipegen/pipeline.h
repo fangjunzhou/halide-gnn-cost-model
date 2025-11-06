33301796d924d0f8ad375e9f1622ed6c1df3eb56
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-11-05 22:18:32 -0800

    Added schedule visitor TODO

--- pipegen/pipeline.h
+++ pipegen/pipeline.h
@@ -61,13 +61,17 @@ class Pipeline {
   nlohmann::json serializeAST();
 
   /**
+   * @brief Serialize the loop nest schedule into a json object.
+   */
+  nlohmann::json serializeSchedule();
+
+  /**
    * @brief Store per-function JSON ASTs created at generation time.
    * Key: function name -> value: JSON AST for that function (or null)
    */
   std::unordered_map<std::string, nlohmann::json> func_asts;
 
   // TODO: Add scheudle serialization.
-
 
  private:
   void topologicalSort();
