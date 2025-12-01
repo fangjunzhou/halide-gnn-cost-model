c1cf048bed3e0432d6dba69fe6b4cc865917c349
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-11-30 19:21:09 -0800

    Added training container

--- scripts/pipegen_array.sh
+++ scripts/pipegen_array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=pipegen
-#SBATCH --array=0-31
-#SBATCH --time=00:10:00
+#SBATCH --array=0-63
+#SBATCH --time=00:45:00
 #SBATCH -c 1
 #SBATCH --mem=8G
 #SBATCH --output=logs/job_%A_%a.log
@@ -12,7 +12,7 @@ rm -rf /var/tmp/build-job${SLURM_ARRAY_TASK_ID}
 
 srun apptainer run -B.:/workspace /home/groups/kayvonf/fzhou48/simg/pipebench.simg \
     --pipeline-id ${SLURM_ARRAY_TASK_ID} \
-    --num-schedules 32 \
+    --num-schedules 64 \
     --pipelines-dir /var/tmp/piplines-job${SLURM_ARRAY_TASK_ID} \
     --build-dir /var/tmp/build-job${SLURM_ARRAY_TASK_ID} -v
 
