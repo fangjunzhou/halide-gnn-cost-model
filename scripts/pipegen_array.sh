8a7eb2b24a5500709aa228b06a7f9bfa175575b6
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-12-01 00:24:56 -0800

    Updated scripts and pipebench

--- scripts/pipegen_array.sh
+++ scripts/pipegen_array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=pipegen
-#SBATCH --array=0-63
+#SBATCH --array=0-128
 #SBATCH --time=00:45:00
-#SBATCH -c 1
-#SBATCH --mem=8G
+#SBATCH -c 8
+#SBATCH --mem=32G
 #SBATCH --output=logs/job_%A_%a.log
 
 
@@ -12,9 +12,11 @@ rm -rf /var/tmp/build-job${SLURM_ARRAY_TASK_ID}
 
 srun apptainer run -B.:/workspace /home/groups/kayvonf/fzhou48/simg/pipebench.simg \
     --pipeline-id ${SLURM_ARRAY_TASK_ID} \
-    --num-schedules 64 \
+    --num-schedules 128 \
     --pipelines-dir /var/tmp/piplines-job${SLURM_ARRAY_TASK_ID} \
     --build-dir /var/tmp/build-job${SLURM_ARRAY_TASK_ID} -v
 
-mkdir -p /home/groups/kayvonf/fzhou48/pipelines
-cp -r /var/tmp/piplines-job${SLURM_ARRAY_TASK_ID}/* /home/groups/kayvonf/fzhou48/pipelines
+mkdir -p /home/groups/kayvonf/fzhou48/pipeline-job-artifacts
+cd /var/tmp/piplines-job${SLURM_ARRAY_TASK_ID}
+tar -cvf piplines-job${SLURM_ARRAY_TASK_ID}.tar *
+cp /var/tmp/piplines-job${SLURM_ARRAY_TASK_ID}/piplines-job${SLURM_ARRAY_TASK_ID}.tar /home/groups/kayvonf/fzhou48/pipeline-job-artifacts
